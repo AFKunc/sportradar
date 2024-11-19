2a659963a6cc1f35564c54ed439c5878caba44ea
Albert Filip Kunc <albertfilip.kunc-ext@hexagon.com>
2024-11-19 09:05:49 +0100

    interface refactor to align with specification + happy path test

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -1,52 +1,105 @@
+#include <algorithm>
 #include <iostream>
+#include <limits>
 #include <map>
 #include <string>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
+using MatchId = uint64_t;
+
 struct Match {
+    MatchId id;
     std::string home_name;
     std::string away_name;
-    int home_score = 0;
-    int away_score = 0;
+    unsigned home_score = 0;
+    unsigned away_score = 0;
 
     uint64_t total_score() const {
         return home_score + away_score;
     }
+
+    bool operator<(const Match& other) const {
+        if (total_score() != other.total_score()) {
+            return total_score() < other.total_score();
+        }
+        return id > other.id;
+    }
 };
 
-std::ostream &operator<<(std::ostream &os, const Match &match) {
+std::ostream& operator<<(std::ostream& os, const Match& match) {
     os << match.home_name << ": " << match.home_score << " - " << match.away_name << ": " << match.away_score;
     return os;
 }
 
 class Scoreboard {
 public:
-    uint64_t add_match(Match match) {
-        static uint64_t id = 0;
-        matches.insert({id, std::move(match)});
-        return id++;
+    MatchId start_match(std::string home_name, std::string away_name) {
+        Match match{next_id, std::move(home_name), std::move(away_name)};
+        matches.insert({next_id, match});
+        return next_id++;
     }
 
-    void summary() {
-        for (const auto &[id, match]: matches) {
-            std::cout << match << std::endl;
+    void update_match(MatchId id, uint64_t home_score, uint64_t away_score) {
+        if (auto match_itr = matches.find(id); match_itr != matches.end()) {
+            match_itr->second.home_score = home_score;
+            match_itr->second.away_score = away_score;
         }
     }
 
+    void remove_match(MatchId id) {
+        matches.erase(id);
+    }
+
+    void end_match(MatchId id) {
+        auto match = matches.find(id);
+        if (match != matches.end()) {
+            match->second.home_score = 0;
+            match->second.away_score = 0;
+        }
+    }
+
+    auto summary() const {
+        std::vector<std::pair<MatchId, Match>> summary(matches.cbegin(), matches.cend());
+        std::sort(summary.begin(), summary.end());
+
+        for (const auto& [_, match]: summary) {
+            std::cout << match << std::endl;
+        }
+
+        return summary;
+    }
+
 private:
-    std::map<uint64_t, Match> matches;
+    MatchId next_id = 0;
+    std::map<MatchId, Match> matches;
 };
 
 int main() {
-    Match match{.home_name = "h1", .away_name = "a1", .home_score = 1, .away_score = 2};
-    std::cout << match << std::endl;
-
+    std::vector<Match> test_matches1{
+            {.home_name = "Mexico", .away_name = "Canada", .home_score = 0, .away_score = 5},
+            {.home_name = "Spain", .away_name = "Brazil", .home_score = 10, .away_score = 2},
+            {.home_name = "Germany", .away_name = "France", .home_score = 2, .away_score = 2},
+            {.home_name = "Uruguay", .away_name = "Italy", .home_score = 6, .away_score = 6},
+            {.home_name = "Argentina", .away_name = "Australia", .home_score = 3, .away_score = 1},
+            //            {.home_name = "NaszeChłopaki", .away_name = "*******!!!", .home_score = std::numeric_limits<int>().max(), .away_score = 0},
+    };
     Scoreboard scoreboard;
-    scoreboard.add_match({.home_name = "h2", .away_name = "a2", .home_score = 0, .away_score = 3});
-
     scoreboard.summary();
 
+    for (const auto& match: test_matches1) {
+        auto id = scoreboard.start_match(match.home_name, match.away_name);
+        scoreboard.update_match(id, match.home_score, match.away_score);
+    }
+    scoreboard.summary();
+
+    std::cout << std::endl;
+    scoreboard.update_match(1, 11, 2);// update mexico vs canada
+    scoreboard.summary();
+
+
+    std::cout << std::endl;
+    scoreboard.remove_match(0);// remove mexico vs canada
+    scoreboard.summary();
     return 0;
 }
